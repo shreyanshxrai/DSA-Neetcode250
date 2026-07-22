e3fe3f8bd684e3fa4206907f9650b10ef272faed
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-07-22 10:05:00 +0530

    added solution

---
+++ array/6-group-anagrams/main.py
@@ -0,0 +1,12 @@
+from collections import defaultdict
+
+
+class solution(object):
+    def groupAnagrams(self , strs):
+        res = defaultdict(list)
+        for s in strs :
+            count = [0] * 26
+            for c in s :
+                count[ord(c) - ord("a")] += 1
+            res[tuple(count)].append(s)  
+        return res.values()      
